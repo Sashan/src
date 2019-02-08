12247d6aac80e0083f937213ac934053bcbca03b
sashan <anedvedicky@gmail.com>
2019-02-08 01:13:54 +0100

    - more debugging stuff

--- sys/kern/kern_mcslock.c
+++ sys/kern/kern_mcslock.c
@@ -55,6 +55,7 @@ mcs_lock_enter(struct mcs_lock *mcs)
 	old_mcs = atomic_swap_ptr(&mcs->mcs_global->mcs_next, mcs);
 	
 	if (old_mcs != NULL) {
+		KASSERT(old_mcs->mcs_owner == mcs->mcs_owner);
 		old_mcs->mcs_next = mcs;
 		membar_exit();
 
@@ -69,9 +70,10 @@ mcs_lock_enter(struct mcs_lock *mcs)
 #ifdef DIAGNOSTIC
 			i--;
 			if (i == 0)
-				panic("%s @ %p infinite spinlock "
+				panic("%s @ %p (%p) infinite spinlock "
 				    "%p/old_mcs %p/mcs\n",
-				    __func__, curproc, old_mcs, mcs);
+				    __func__, curproc, old_mcs->mcs_owner,
+				    old_mcs, mcs);
 #endif
 		}
 	}
@@ -111,14 +113,15 @@ mcs_lock_leave(struct mcs_lock *mcs)
 #ifdef DIAGNOSTIC
 		i--;
 		if (i == 0)
-			panic("%s @ %p infinite spinlock %p/mcs\n",
-			    __func__, curproc, mcs);
+			panic("%s @ %p (%p) infinite spinlock %p/mcs\n",
+			    __func__, curproc, mcs->mcs_owner, mcs);
 #endif
 	}
 
 	/*
 	 * let our waiter run.
 	 */
+	KASSERT(mcs->mcs_owner == mcs_next->mcs_owner);
 	mcs->mcs_next->mcs_wait = NULL;
 	membar_exit();
 }
