9d2092736f8674c0daf3adf1dd219bccd47f5bde
sashan <anedvedicky@gmail.com>
2019-05-30 23:42:06 +0200

    - fix races on state key

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -7367,11 +7367,20 @@ pf_inp_unlink(struct inpcb *inp)
 void
 pf_state_key_link_reverse(struct pf_state_key *sk, struct pf_state_key *skrev)
 {
-	/* Note that sk and skrev may be equal, then we refcount twice. */
-	KASSERT(sk->reverse == NULL);
-	KASSERT(skrev->reverse == NULL);
-	sk->reverse = pf_state_key_ref(skrev);
-	skrev->reverse = pf_state_key_ref(sk);
+	struct pf_state_key *old_reverse;
+
+	old_reverse = atomic_cas_ptr(&sk->reverse, NULL, skrev);
+	if (old_reverse != NULL)
+		KASSERT(old_reverse == skrev);
+	else
+		pf_state_key_ref(skrev);
+
+
+	old_reverse = atomic_cas_ptr(&skrev->reverse, NULL, sk);
+	if (old_reverse != NULL)
+		KASSERT(old_reverse == sk);
+	else
+		pf_state_key_ref(sk);
 }
 
 #if NPFLOG > 0
