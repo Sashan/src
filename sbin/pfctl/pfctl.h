aa529cda64d86e0b8d58f2a91a6abf23727cba0b
sashan <anedvedicky@gmail.com>
2024-07-14 10:57:53 +0200

    - fix flaw pointed out by bluhm@

--- sbin/pfctl/pfctl.h
+++ sbin/pfctl/pfctl.h
@@ -70,6 +70,7 @@ struct pfr_uktable {
 
 #define pfrukt_t	pfrukt_kt.pfrkt_ts.pfrts_t
 #define pfrukt_name	pfrukt_kt.pfrkt_t.pfrt_name
+#define pfrukt_anchor	pfrukt_kt.pfrkt_t.pfrt_anchor
 
 extern struct pfr_ktablehead pfr_ktables;
 
