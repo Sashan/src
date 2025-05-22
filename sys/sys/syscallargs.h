316b93e473afe5d32e048b5bb19f32479cd1fc1d
sashan <anedvedicky@gmail.com>
2025-05-22 14:21:04 +0200

    - files generated by make syscalls

--- sys/sys/syscallargs.h
+++ sys/sys/syscallargs.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syscallargs.h,v 1.285 2025/02/17 13:10:56 mpi Exp $	*/
+/*	$OpenBSD$	*/
 
 /*
  * System call argument lists.
@@ -963,6 +963,10 @@ struct sys_fhopen_args {
 	syscallarg(int) flags;
 };
 
+struct sys_set_shlibinfo_args {
+	syscallarg(const void *) shlibinfo;
+};
+
 struct sys_kqueue1_args {
 	syscallarg(int) flags;
 };
@@ -1361,6 +1365,7 @@ int	sys_getsid(struct proc *, void *, register_t *);
 int	sys_msync(struct proc *, void *, register_t *);
 int	sys_pipe(struct proc *, void *, register_t *);
 int	sys_fhopen(struct proc *, void *, register_t *);
+int	sys_set_shlibinfo(struct proc *, void *, register_t *);
 int	sys_kqueue(struct proc *, void *, register_t *);
 int	sys_kqueue1(struct proc *, void *, register_t *);
 int	sys_mlockall(struct proc *, void *, register_t *);
