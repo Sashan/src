e3f45a5811f41bd172af76178af695dd6380af82
sashan <anedvedicky@gmail.com>
2025-05-25 07:05:56 +0200

    - code compiles, btrace crashes

--- usr.sbin/btrace/btrace.c
+++ usr.sbin/btrace/btrace.c
@@ -39,6 +39,8 @@
 
 #include <dev/dt/dtvar.h>
 
+#include <gelf.h>
+
 #include "btrace.h"
 #include "bt_parser.h"
 
@@ -111,6 +113,8 @@ void			 debug_dump_term(struct bt_arg *);
 void			 debug_dump_expr(struct bt_arg *);
 void			 debug_dump_filter(struct bt_rule *);
 
+struct syms		*dt_load_syms(pid_t, void *, struct syms *);
+
 struct dtioc_probe_info	*dt_dtpis;	/* array of available probes */
 size_t			 dt_ndtpi;	/* # of elements in the array */
 struct dtioc_arg_info  **dt_args;	/* array of probe arguments */
@@ -120,11 +124,13 @@ struct dt_evt		 bt_devt;	/* fake event for BEGIN/END */
 #define EVENT_END	 (unsigned int)(-1)
 uint64_t		 bt_filtered;	/* # of events filtered out */
 
-struct syms		*kelf, *uelf;
+struct syms		*kelf;
 
+static pid_t		 pid = -1;
 char			**vargs;
 int			 nargs = 0;
 int			 verbose = 0;
+int			 is_dynamic_elf = 0;
 int			 dtfd;
 volatile sig_atomic_t	 quit_pending;
 
@@ -142,6 +148,7 @@ main(int argc, char *argv[])
 	const char *filename = NULL, *btscript = NULL;
 	int showprobes = 0, noaction = 0;
 	size_t btslen = 0;
+	const char *exec_path = NULL;
 
 	setlocale(LC_ALL, "");
 
@@ -158,7 +165,7 @@ main(int argc, char *argv[])
 			noaction = 1;
 			break;
 		case 'p':
-			uelf = kelf_open(optarg);
+			exec_path = optarg;
 			break;
 		case 'v':
 			verbose++;
@@ -212,6 +219,9 @@ main(int argc, char *argv[])
 		if (fd == -1)
 			err(1, "could not open %s", __PATH_DEVDT);
 		dtfd = fd;
+
+		if (argv[0] != 0)
+			pid = strtonum(argv[0], 0, INT_MAX, NULL);
 	}
 
 	if (showprobes) {
@@ -611,7 +621,7 @@ rules_setup(int fd)
 	}
 
 	if (dokstack)
-		kelf = kelf_open(_PATH_KSYMS);
+		kelf = kelf_open_kernel(_PATH_KSYMS);
 
 	/* Initialize "fake" event for BEGIN/END */
 	bt_devt.dtev_pbn = EVENT_BEGIN;
@@ -699,9 +709,6 @@ rules_teardown(int fd)
 	}
 
 	kelf_close(kelf);
-	kelf = NULL;
-	kelf_close(uelf);
-	uelf = NULL;
 
 	/* Update "fake" event for BEGIN/END */
 	bt_devt.dtev_pbn = EVENT_END;
@@ -829,10 +836,10 @@ builtin_stack(struct dt_evt *dtev, int kernel, unsigned long offset)
 		int l;
 
 		if (!kernel)
-			l = kelf_snprintsym(uelf, bp, sz - 1, st->st_pc[i],
-			    offset);
+			l = kelf_snprintsym_proc(dtfd, pid, bp, sz - 1,
+			    st->st_pc[i], offset);
 		else
-			l = kelf_snprintsym(kelf, bp, sz - 1, st->st_pc[i],
+			l = kelf_snprintsym_kernel(kelf, bp, sz - 1, st->st_pc[i],
 			    offset);
 		if (l < 0)
 			break;
@@ -1808,7 +1815,10 @@ ba2str(struct bt_arg *ba, struct dt_evt *dtev)
 		str = builtin_stack(dtev, 1, 0);
 		break;
 	case B_AT_BI_USTACK:
-		str = builtin_stack(dtev, 0, dt_get_offset(dtev->dtev_pid));
+		if (is_dynamic_elf)
+			str = builtin_stack(dtev, 0, 0);
+		else
+			str = builtin_stack(dtev, 0, dt_get_offset(dtev->dtev_pid));
 		break;
 	case B_AT_BI_COMM:
 		str = dtev->dtev_comm;
